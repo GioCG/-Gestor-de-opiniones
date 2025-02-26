11f955717bb4894c2d52d6a8798d2d2a13ea2145
GioCG <gcarrera-2020313@kinal.edu.gt>
2025-02-25 22:06:18 -0600

    se agregaron funcionalidades comment

---
+++ src/commit/commit.controler.js
@@ -0,0 +1,14 @@
+import Publicacion from '../publicaciones/publicacion.model.js'
+import Commit from '../commit/commit.model.js'
+
+export const addCommit = async (req, res) => {
+
+}
+
+export const editCommit = async (req, res) => {
+
+}
+
+export const deletCommit = async (req, res) => {
+
+}
